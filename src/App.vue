f37775d16cff3379a25037e867456b3104c29e55
myomi <myomit@gmail.com>
2017-09-02 11:02:57 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <button class="btn btn-primary">hello</button>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
